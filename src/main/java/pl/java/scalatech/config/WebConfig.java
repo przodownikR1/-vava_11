2d6b614c51bd92cb19134265da669d354123491e
Slawomir Borowiec <przodownik@tlen.pl>
2015-11-25 12:57:04 +0100

    webjars

--- src/main/java/pl/java/scalatech/config/WebConfig.java
+++ src/main/java/pl/java/scalatech/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
@@ -20,6 +21,18 @@ public class WebConfig  extends WebMvcConfigurerAdapter{
         registration.addUrlMappings("/console/*");
         registration.addInitParameter("webAllowOthers", "true");
         return registration;
+    }
+    
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/META-INF/resources/webjars/").setCachePeriod(3000);
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/css/").setCachePeriod(0);
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/resources/images/").setCachePeriod(3000);
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/static/favicon.ico").setCachePeriod(3000);
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/").setCachePeriod(3000);
+
     }
     
     @Override
