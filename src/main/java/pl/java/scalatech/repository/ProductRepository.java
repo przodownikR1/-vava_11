b6b5356f678a857bd367797a7c92802b340f5cc7
Slawomir Borowiec <przodownik@tlen.pl>
2015-11-27 02:11:37 +0100

    a few new features

--- src/main/java/pl/java/scalatech/repository/ProductRepository.java
+++ src/main/java/pl/java/scalatech/repository/ProductRepository.java
@@ -10,9 +10,14 @@ import org.springframework.transaction.annotation.Transactional;
 import pl.java.scalatech.entity.Product;
 import pl.java.scalatech.entity.User;
 
+
 @Transactional(propagation=Propagation.REQUIRES_NEW,noRollbackFor=IllegalArgumentException.class)
 public interface ProductRepository  extends JpaRepository<Product, Long>{
 
     List<Product> findByOwner(User user);
+
+    @Override
+   // @PostAuthorize("hasPermission(returnObject?.owner?.id = principal?.id )")
+    Product findOne(Long id);
 
 }
