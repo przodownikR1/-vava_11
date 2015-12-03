c997a23450315131175389f171526fe9d64c5bdf
Slawomir Borowiec <przodownik@tlen.pl>
2015-12-03 15:59:49 +0100

    currentUser work

--- src/main/java/pl/java/scalatech/config/SecurityBasicConfig.java
+++ src/main/java/pl/java/scalatech/config/SecurityBasicConfig.java
@@ -5,8 +5,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.access.PermissionEvaluator;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -38,7 +42,21 @@ public class SecurityBasicConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private LogoutSuccessHandler logoutSuccessHander;
+    
+    @Configuration
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    static class MethodSecurityConfiguration extends GlobalMethodSecurityConfiguration {
+        @Autowired
+        private PermissionEvaluator permissionEvaluator;
 
+        @Override
+        protected MethodSecurityExpressionHandler createExpressionHandler() {
+            DefaultMethodSecurityExpressionHandler handler =
+                    new DefaultMethodSecurityExpressionHandler();
+            handler.setPermissionEvaluator(permissionEvaluator);
+            return handler;
+        }
+    }
 
   /*  @Configuration
     @EnableGlobalMethodSecurity(prePostEnabled = true)
