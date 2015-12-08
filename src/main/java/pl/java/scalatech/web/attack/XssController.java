367710668064b3b85043041d387480c1c46c267c
Slawomir Borowiec <przodownik@tlen.pl>
2015-12-08 16:16:12 +0100

    xss first

--- src/main/java/pl/java/scalatech/web/attack/XssController.java
+++ src/main/java/pl/java/scalatech/web/attack/XssController.java
@@ -4,11 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class XssController {
     @RequestMapping("/xss")
     String xss(Model model){
-        model.addAttribute("msg", "js!!");
+        model.addAttribute("msg", "Hi : <script>alert(document.cookie)</script>");
+        log.info("model   :  {}",model);
         return "xss";
         
     }
